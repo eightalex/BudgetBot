a275dda4cb8be0dba6e6c290fc214b7e25e5fbdb
o.kuzmenko <ok@consulting.mk>
2019-09-11 23:55:13 +0300

    Refactor TelegramAdapter

--- src/components/TelegramAdapter.js
+++ src/components/TelegramAdapter.js
@@ -3,6 +3,7 @@ export default class TelegramAdapter {
     constructor() {
         this.token = process.env.TELEGRAM_TOKEN;
         this.googleAppUrl = process.env.GOOGLE_APP_URL;
+        this.apiUrl = 'https://api.telegram.org/bot';
     }
 
     /**
@@ -11,7 +12,7 @@ export default class TelegramAdapter {
      * @return {Promise<Response>}
      */
     post(method, params) {
-        const url = 'https://api.telegram.org/bot' + this.token + '/' + method;
+        const url = this.apiUrl + this.token + '/' + method;
         return UrlFetchApp.fetch(url, {'method': 'post', 'payload': params});
     }
 
@@ -28,7 +29,7 @@ export default class TelegramAdapter {
      * @return {Promise<Response>}
      */
     setWebhook() {
-        return UrlFetchApp.fetch('https://api.telegram.org/bot' + this.token + "/setWebhook?url=" + this.googleAppUrl);
+        return UrlFetchApp.fetch(this.apiUrl + this.token + '/setWebhook?url=' + this.googleAppUrl);
     }
 
 }
