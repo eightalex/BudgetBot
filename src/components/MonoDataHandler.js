e949df94f6eaf62e08f24939a99dc71114ec1ba8
o.kuzmenko <eight.alex@gmail.com>
2019-10-03 11:27:18 +0300

    Added telegram notification when mono transaction handled

--- src/components/MonoDataHandler.js
+++ src/components/MonoDataHandler.js
@@ -2,15 +2,22 @@ export default class MonoDataHandler {
 
     /**
      * @param {Budget} budget
+     * @param {TelegramAdapter} telegram
      * @param {NumberHandler} numberHandler
      * @param {MessageHandler} messageHandler
+     * @param {MessageGenerator} messageGenerator
      */
-    constructor(budget, numberHandler, messageHandler) {
+    constructor(budget, telegram, numberHandler, messageHandler, messageGenerator) {
         this.budget = budget;
+        this.telegram = telegram;
         this.numberHandler = numberHandler;
         this.messageHandler = messageHandler;
+        this.messageGenerator = messageGenerator;
     }
 
+    /**
+     * @param {json} contents
+     */
     handle(contents) {
         const amount = this.numberHandler.prepareMonoAmount(contents.data.statementItem.amount);
         const isIncome = amount > 0;
@@ -18,6 +25,7 @@ export default class MonoDataHandler {
         const comment = this.messageHandler.prepareComment(contents.data.statementItem.description, value);
 
         this.budget.setTransaction({comment, value});
+        this.telegram.message(process.env.CHAT_ID, this.messageGenerator.getMessage('today'));
     }
 
 }
