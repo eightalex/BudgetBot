526c5f5b743ba763ce0080e53c2396e425d6ca2e
o.kuzmenko <ok@consulting.mk>
2021-03-13 20:17:58 +0200

    Updated current day marker

---
+++ src/config/currentDayMark.ts
@@ -0,0 +1,13 @@
+export const currentDayMark: {
+    value: string
+    color: string
+    font: {
+        weight: GoogleAppsScript.Spreadsheet.FontWeight
+    }
+} = {
+    value: '>',
+    color: '#6aa84f',
+    font: {
+        weight: 'bold',
+    },
+};
--- src/services/Table.ts
+++ src/services/Table.ts
@@ -1,9 +1,8 @@
 import {cells} from '~/config/cells';
+import {currentDayMark} from '~/config/currentDayMark';
 import {STRING} from '~/constants/string';
 import {SpreadsheetAppAdapterInterface} from './SpreadsheetAppAdapterInterface';
 import {TableInterface} from './TableInterface';
-
-const MARK = '>';
 
 export class Table implements TableInterface {
     constructor(
@@ -12,21 +11,27 @@ export class Table implements TableInterface {
 
     markCurrentDay(): void {
         const currentDate = this.spreadsheetAppAdapter.getCell(cells.mark);
-        this.clearAllMarks();
-        currentDate.setValue(MARK);
+
+        this.resetMarksColumn();
+
+        currentDate
+            .setValue(currentDayMark.value)
+            .setFontColor(currentDayMark.color)
+            .setFontWeight(currentDayMark.font.weight);
     }
 
-    private clearAllMarks(): void {
-        const rowStart = Number(process.env.POSITION_ROW_START)
+    private resetMarksColumn(): void {
+        const rowStart = Number(process.env.POSITION_ROW_START);
         const rowEnd = Number(process.env.POSITION_ROW_END);
+        const column = Number(process.env.POSITION_COLUMN_MARK);
 
-        for (let i = rowStart; i < rowEnd; i++) {
-            const cell = this.spreadsheetAppAdapter.getCell({
-                position: {
-                    row: i,
-                    column: Number(process.env.POSITION_COLUMN_MARK)
-                }
-            });
+        let row: number;
+
+        for (row = rowStart; row < rowEnd; row++) {
+            const cell = this.spreadsheetAppAdapter.getCell({position: {row, column}});
+
+            // @ts-ignore waiting for fix https://github.com/DefinitelyTyped/DefinitelyTyped/pull/51727
+            cell.setHorizontalAlignment(DocumentApp.HorizontalAlignment.RIGHT);
             cell.setValue(STRING.EMPTY);
         }
     }
